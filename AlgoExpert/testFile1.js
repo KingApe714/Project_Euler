1650eb8670f851cf1611bb0b8bf54803fcd89cb6
jonathandiaz <jonathan.diaz714@gmail.com>
2022-03-02 16:40:05 -0500

    complete sortedSquaredArray

---
+++ AlgoExpert/testFile1.js
@@ -0,0 +1,10 @@
+function sortedSquaredArray(array) {
+    // Write your code here.
+      let arr = []
+      
+      array.forEach(ele => {
+          arr.push(ele * ele)
+      })
+      
+    return arr.sort((a, b) => a - b);
+  }
