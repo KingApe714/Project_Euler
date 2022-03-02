795144ceb00cecc190669473163a3c07037b9a30
jonathandiaz <jonathan.diaz714@gmail.com>
2022-03-02 16:42:03 -0500

    complete twoSum

--- AlgoExpert/testFile1.js
+++ AlgoExpert/testFile1.js
@@ -8,3 +8,20 @@ function sortedSquaredArray(array) {
       
     return arr.sort((a, b) => a - b);
   }
+
+  function twoNumberSum(array, targetSum) {
+    // Write your code here.
+      let obj = {};
+      
+      for (let i = 0; i < array.length; i++) {
+          let num1 = array[i]
+          //now create the variable that we are checking for
+          let num2 = targetSum - num1
+          //now check to see if num2 is in the hash
+          if (obj[num2]) return [num1, num2]
+          
+          obj[num1] = true
+      }
+      
+      return [];
+  }
