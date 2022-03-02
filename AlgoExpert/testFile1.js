eda85cb3ed0410ac9c530bb438b84a7e9fea95a8
jonathandiaz <jonathan.diaz714@gmail.com>
2022-03-02 16:45:32 -0500

    completed tournament winner

--- AlgoExpert/testFile1.js
+++ AlgoExpert/testFile1.js
@@ -43,3 +43,33 @@ function sortedSquaredArray(array) {
       
       return false;
   }
+
+  function tournamentWinner(competitions, results) {
+    // Write your code here.
+      //set up an object with each team name as the key and
+      //their corresponding scores as the value.
+      let obj = {};
+      let winner = ""
+      let highest = 0
+      
+      for (let i = 0; i < results.length; i++) {
+          let home = competitions[i][0]
+          let away = competitions[i][1]
+          let res = results[i]
+          
+          if (!obj[home]) obj[home] = 0
+          if (!obj[away]) obj[away] = 0
+          
+          if (res === 0) obj[away] += 3
+          if (res === 1) obj[home] += 3
+      }
+      
+      for (let team in obj) {
+          let score = obj[team];
+          if (score > highest) {
+              highest = score;
+              winner = team
+          }
+      }
+    return winner;
+  }
