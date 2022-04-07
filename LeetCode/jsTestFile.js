e72a4e79cebf1b2d436334d32c186b7003d89d51
jonathandiaz <jonathan.diaz714@gmail.com>
2022-04-07 13:03:01 -0400

    solve maxArea problem

--- LeetCode/jsTestFile.js
+++ LeetCode/jsTestFile.js
@@ -104,3 +104,19 @@ var lengthOfLongestSubstring = function(s) {
 
 	return merged;
 };
+
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+    while(right > left) {
+          maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return maxArea
+};
