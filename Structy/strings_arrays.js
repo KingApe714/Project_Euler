8bae391cdbb5bba5e028795dfecd6db7990f345e
Jonathan <jonathan.diaz714@gmail.com>
2022-08-24 16:44:51 -0400

    compress complete

--- Structy/strings_arrays.js
+++ Structy/strings_arrays.js
@@ -18,3 +18,23 @@ const uncompress = (s) => {
     
     return str;
   };
+
+  const compress = (s) => {
+    // todo
+    let str = '';
+    for (let i = 0; i < s.length; i++) {
+      let count = 1;
+      let chr = s[i];
+      while (s[i] === s[i + 1]) {
+        count++;
+        i++;
+      }
+      if (count > 1) {
+        str += count + chr;
+      } else {
+        str += chr;
+      }
+    }
+    
+    return str;
+  };
