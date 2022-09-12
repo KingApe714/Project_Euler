2aa7016e0c0d14a4bbe5ff4e81aa7e6253660264
Jonathan <jonathan.diaz714@gmail.com>
2022-09-11 21:55:52 -0400

    solved the recursive solution for the hasPath problem

--- Structy/graph.js
+++ Structy/graph.js
@@ -54,3 +54,12 @@ const hasPath = (graph, src, dst) => {
 
     return false;
 };
+
+const hasPath2 = (graph, src, dst) => {
+    if (src === dst) return true;
+    for (let neighbor of graph[src]) {
+        if (hasPath(graph, neighbor, dst)) return true;
+    }
+
+    return false;
+};
