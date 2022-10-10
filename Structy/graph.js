badd44bde06a6f8e9f691004491c82d415a77e18
Jonathan <jonathan.diaz714@gmail.com>
2022-10-10 16:50:20 -0400

    solved the bestBridge problem

--- Structy/graph.js
+++ Structy/graph.js
@@ -313,3 +313,64 @@ const buildGraph1 = (numCourses, prereqs) => {
     }
     return graph;
 }
+
+const bestBridge = (grid) => {
+    let mainIsland;
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            const potentialIsland = traverseIsland(grid, r, c, new Set());
+            if (potentialIsland.size > 0) {
+                mainIsland = potentialIsland;
+                break;
+            }
+        }
+    }
+
+    const visited = new Set(mainIsland)
+    const queue = [];
+    for (let pos of mainIsland) {
+        const [r, c] = pos.split(',').map(Number);
+        queue.push([r, c, 0])
+    }
+
+    while (queue.length > 0) {
+        const [ r, c, distance ] = queue.shift();
+        const pos = r + ',' + c;
+        if (grid[r][c] === 'L' && !mainIsland.has(pos)) {
+            return distance - 1;
+        }
+        
+        let deltas = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+        for (let delta of deltas) {
+            let [dRow, dCol] = delta;
+            let nRow = r + dRow;
+            let nCol = c + dCol;
+            let nPos = nRow + ',' + nCol
+            if (isInbounds(grid, nRow, nCol) && !visited.has(nPos)) {
+                visited.add(nPos)
+                queue.push([nRow, nCol, distance + 1]);
+            }
+        }
+    }
+};
+
+const isInbounds = (grid, r, c) => {
+    const rowInbounds = 0 <= r && r < grid.length;
+    const colInbounds = 0 <= c && c < grid[0].length;
+    return rowInbounds && colInbounds;
+}
+
+    const traverseIsland = (grid, r, c, visited) => {
+    if (!isInbounds(grid, r, c) || grid[r][c] === "W") return visited;
+
+    const pos = r + ',' + c;
+    if (visited.has(pos)) return visited;
+    visited.add(pos);
+
+    traverseIsland(grid, r - 1, c, visited);
+    traverseIsland(grid, r + 1, c, visited);
+    traverseIsland(grid, r, c - 1, visited);
+    traverseIsland(grid, r, c + 1, visited);
+
+    return visited;
+}
