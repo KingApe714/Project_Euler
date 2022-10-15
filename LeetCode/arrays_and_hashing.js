a5b3313042210790a22778123d916d9e3cb97002
Jonathan <jonathan.diaz714@gmail.com>
2022-10-15 19:00:43 -0400

    solved the anagram problem

--- LeetCode/arrays_and_hashing.js
+++ LeetCode/arrays_and_hashing.js
@@ -7,3 +7,20 @@ var containsDuplicate = function(nums) {
     
     return false;
 };
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let obj = {};
+    for (let i = 0; i < s.length; i++) {
+        let chr1 = s[i];
+        let chr2 = t[i];
+        if (!(chr1 in obj)) obj[chr1] = 0;
+        obj[chr1]++;
+        if (!(chr2 in obj)) obj[chr2] = 0;
+        obj[chr2]--;
+    }
+    for (let key in obj) {
+        if (obj[key] !== 0) return false;
+    }
+    return true;
+};
