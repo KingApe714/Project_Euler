16834abe8c5f322fab55e0d6176ef41790e9e29e
Jonathan <jonathan.diaz714@gmail.com>
2022-10-18 17:19:55 -0400

    solved the reverseString problem

--- LeetCode/two_pointers.js
+++ LeetCode/two_pointers.js
@@ -31,3 +31,16 @@ var minimumDifference = function(nums, k) {
     }
     return min;
 };
+
+var reverseString = function(s) {
+    let i = 0;
+    let j = s.length - 1;
+    
+    while (i < j) {
+        [s[i], s[j]] = [s[j], s[i]]
+        i++;
+        j--;
+    }
+    
+    return s;
+};
