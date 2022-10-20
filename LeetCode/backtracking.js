1b49036345de9235f7ba363fe0ae4acf98157d47
Jonathan <jonathan.diaz714@gmail.com>
2022-10-20 14:52:19 -0400

    stringSearch solved

--- LeetCode/backtracking.js
+++ LeetCode/backtracking.js
@@ -22,3 +22,33 @@ var permute = function(nums) {
     
     return arr
 };
+
+const stringSearch = (grid, s) => {
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (dfs(grid, s, i, j)) return true;
+      }
+    }
+    
+    return false;
+  };
+  
+  const dfs = (grid, s, i, j) => {
+    if (s.length === 0) return true;
+    
+    let rowInbounds = i >= 0 && i < grid.length;
+    let colInbounds = j >= 0 && j < grid[0].length;
+    
+    if (!rowInbounds || !colInbounds) return false;
+    let chr = grid[i][j];
+    if (s[0] !== chr) return false;
+    
+    grid[i][j] = '*'
+    let res = dfs(grid, s.slice(1), i + 1, j) ||
+              dfs(grid, s.slice(1), i - 1, j) ||
+              dfs(grid, s.slice(1), i, j + 1) ||
+              dfs(grid, s.slice(1), i, j - 1);
+    grid[i][j] = chr;
+    
+    return res;
+}
