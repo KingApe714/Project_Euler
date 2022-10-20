c8667800eca98a85a87a41704037dda2b8fc2c6a
Jonathan <jonathan.diaz714@gmail.com>
2022-10-19 20:07:20 -0400

    solved the lengthOfLongestSubstring problem

--- LeetCode/sliding_window.js
+++ LeetCode/sliding_window.js
@@ -8,3 +8,24 @@ var maxProfit = function(prices) {
     
     return max
 };
+
+var lengthOfLongestSubstring = function(s) {
+    let result = 0;
+    let current = "";
+    let longest = "";
+    
+    // abcdaeskdnvknsdk
+    
+    for (let i = 0; i < s.length; i++) {
+        let hasChar = current.includes(s[i])
+        if (hasChar) {
+            let pos = current.indexOf(s[i])
+            current = current.slice(pos + 1);
+        }
+        current += s[i];
+        result = Math.max(current.length, result)
+    }
+    
+    
+    return result
+};
