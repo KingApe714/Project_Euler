6d6bc76b173e1e4fed24dd8520a1c481dc23b54e
Jonathan <jonathan.diaz714@gmail.com>
2022-10-21 18:41:56 -0400

    solved the maxArea problem

--- LeetCode/two_pointers.js
+++ LeetCode/two_pointers.js
@@ -44,3 +44,19 @@ var reverseString = function(s) {
     
     return s;
 };
+
+const maxArea = height => {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+    while(right > left) {
+        max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return max
+};
