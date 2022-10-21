c526e1368db5f3ddb9c37d94e6307cfe511bbb6a
Jonathan <jonathan.diaz714@gmail.com>
2022-10-21 14:51:00 -0400

    solved the mergeList problem

--- LeetCode/linked_list.js
+++ LeetCode/linked_list.js
@@ -11,3 +11,26 @@ const reverseList = head => {
     
     return prev;
 };
+
+const mergeTwoLists = (list1, list2) => {
+    let dummyHead = new ListNode(null);
+    let tail = dummyHead;
+    let current1 = list1;
+    let current2 = list2;
+    
+    while (current1 && current2) {
+        if (current1.val < current2.val) {
+            tail.next = current1
+            current1 = current1.next;
+        } else {
+            tail.next = current2;
+            current2 = current2.next;
+        }
+        tail = tail.next;
+    }
+    
+    if (current1) tail.next = current1;
+    if (current2) tail.next = current2;
+    
+    return dummyHead.next;
+};
