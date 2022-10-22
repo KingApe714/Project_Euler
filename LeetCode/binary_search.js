005e05704dbe325b3af8c2cd7a2fc46c098eedb4
Jonathan <jonathan.diaz714@gmail.com>
2022-10-21 23:02:12 -0400

    solved the searchInsert problem

--- LeetCode/binary_search.js
+++ LeetCode/binary_search.js
@@ -13,3 +13,20 @@ const search = (nums, target) => {
         return idx === -1 ? -1 : mid + idx + 1;
     }
 };
+
+const searchInsert = (nums, target) => {
+    if (nums.length === 0) return 0;
+    
+    let mid = Math.floor(nums.length / 2);
+    let left = nums.slice(0, mid);
+    let right = nums.slice(mid + 1);
+    
+    if (target === nums[mid]) {
+        return mid;
+    } else if (target < nums[mid]) {
+        return searchInsert(left, target);
+    } else {
+        let res = searchInsert(right, target);
+        return res + mid + 1;
+    }
+};
