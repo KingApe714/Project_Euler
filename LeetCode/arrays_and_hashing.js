523532e90ca899aab6899956bd4bbbefe6b32cbc
Jonathan <jonathan.diaz714@gmail.com>
2022-10-22 16:13:45 -0400

    solved the merge sorted arrays problem

--- LeetCode/arrays_and_hashing.js
+++ LeetCode/arrays_and_hashing.js
@@ -258,3 +258,26 @@ const pivotIndex = nums => {
     
     return -1;
 };
+
+const merge = (nums1, m, nums2, n) => {
+    let i = m - 1;
+    let j = n - 1;
+    let k = nums1.length - 1;
+    
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            [nums1[k], nums1[i]] = [nums1[i], nums1[k]];
+            i--;
+        } else {
+            [nums1[k], nums2[j]] = [nums2[j], nums1[k]];
+            j--;
+        }
+        k--;
+    }
+    
+    while (j >= 0) {
+        [nums1[k], nums2[j]] = [nums2[j], nums1[k]];
+        j--;
+        k--;
+    }
+}
