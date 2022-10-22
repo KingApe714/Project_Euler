153d79d988746155a47d7b62a9294d2ae1364928
Jonathan <jonathan.diaz714@gmail.com>
2022-10-22 18:28:24 -0400

    solved validPalindrome

--- LeetCode/two_pointers.js
+++ LeetCode/two_pointers.js
@@ -60,3 +60,30 @@ const maxArea = height => {
     
     return max
 };
+
+const validPalindrome = function(s) {
+    let i = 0;
+    let j = s.length - 1;
+    
+    while (i < j) {
+        if (s[i] === s[j]) {
+            i++;
+            j--;
+        } else {
+            return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        }
+    }
+    
+    return true;
+};
+
+const isPalindrome = (s, i, j) => {
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return false
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
