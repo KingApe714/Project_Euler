cbec513a62a3f6262252a79d73d2f8d59888086a
Jonathan <jonathan.diaz714@gmail.com>
2022-10-21 22:52:59 -0400

    solveed the befitting brackets problem

--- Structy/stack.js
+++ Structy/stack.js
@@ -14,3 +14,26 @@ const pairedParentheses = (str) => {
     
     return count === 0;
 };
+
+const befittingBrackets = str => {
+    const stack = [];
+    const obj = {
+      '{': '}',
+      '[': ']',
+      '(': ')'
+    };
+    
+    for (let char of str) {
+      if (char in obj) {
+        stack.push(obj[char]);
+      } else {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+          stack.pop()
+        } else {
+          return false;
+        }
+      }
+    }
+    
+    return stack.length === 0;
+};
