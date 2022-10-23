4a97212ce0d27031ba928288d47cb3f0f1e5368c
Jonathan <jonathan.diaz714@gmail.com>
2022-10-22 22:55:58 -0400

    solved the maxAreaOfIsland problem

--- LeetCode/graphs.js
+++ LeetCode/graphs.js
@@ -46,3 +46,34 @@ const explore = (grid, i, j, visited) => {
     
     return true;
 }
+
+const maxAreaOfIsland = grid => {
+    let visited = new Set();
+    let max = 0;
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            let res = explore1(grid, i, j, visited);
+            if (res > max) max = res;
+        }
+    }
+    
+    return max;
+};
+
+const explore1 = (grid, i, j, visited) => {
+    if (visited.has(`${i},${j}`)) return 0;
+    if (i < 0 || i >= grid.length
+       || j < 0 || j >= grid[0].length) return 0;
+    if (grid[i][j] === 0) return 0;
+    
+    visited.add(`${i},${j}`);
+    let count = grid[i][j] === 1 ? 1 : 0;
+    
+    count += explore1(grid, i + 1, j, visited);
+    count += explore1(grid, i - 1, j, visited);
+    count += explore1(grid, i, j + 1, visited);
+    count += explore1(grid, i, j - 1, visited);
+    
+    return count;
+}
