129dc0c70c64ed4a1ef331e10d5760319ace6d3c
Jonathan <jonathan.diaz714@gmail.com>
2022-10-25 06:19:34 -0400

    solved the inorderTraversal problem

--- LeetCode/trees.js
+++ LeetCode/trees.js
@@ -4,3 +4,17 @@ const isSameTree = (p, q) => {
     
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
+
+const inorderTraversal = root => {
+    let arr = [];
+    
+    const dfs = root => {
+        if (!root) return
+        dfs(root.left);
+        arr.push(root.val);
+        dfs(root.right);
+    }
+    
+    dfs(root);
+    return arr;
+};
