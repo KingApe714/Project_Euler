4bb73dd59243a253bea5f15ce37085f99f12c9ff
Jonathan <jonathan.diaz714@gmail.com>
2022-10-25 17:23:07 -0400

    solved the isSubtree problem

--- LeetCode/trees.js
+++ LeetCode/trees.js
@@ -18,3 +18,27 @@ const inorderTraversal = root => {
     dfs(root);
     return arr;
 };
+
+const isSubtree = (root, subRoot) => {
+    let stack = [root];
+    
+    while (stack.length) {
+        let curRoot = stack.pop();
+        
+        if (curRoot.val === subRoot.val && equalTree(curRoot, subRoot)) {
+            return true;
+        }
+        
+        if (curRoot.left) stack.push(curRoot.left)
+        if (curRoot.right) stack.push(curRoot.right)
+    }
+    
+    return false;
+}
+
+const equalTree = (a, b) => {
+    if (!a || !b) return !a && !b;
+    if (a.val !== b.val) return false;
+    
+    return equalTree(a.left, b.left) && equalTree(a.right, b.right)
+}
