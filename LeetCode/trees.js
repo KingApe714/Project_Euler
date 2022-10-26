54c88b94ffb7809ac5820c84110b353231491076
Jonathan <jonathan.diaz714@gmail.com>
2022-10-25 22:14:49 -0400

    solved the merge trees problem

--- LeetCode/trees.js
+++ LeetCode/trees.js
@@ -63,3 +63,14 @@ const hasPathSum = (root, targetSum) => {
     
     return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
 };
+
+const mergeTrees = (root1, root2) => {
+    if (!root1) return root2;
+    else if (!root2) return root1;
+    else {
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1
+    }
+};
