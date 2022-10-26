5e3dea65ba80aba0f9844ff6844cc6007826e18e
Jonathan <jonathan.diaz714@gmail.com>
2022-10-25 22:46:17 -0400

    solved the remove element problem

--- LeetCode/arrays_and_hashing.js
+++ LeetCode/arrays_and_hashing.js
@@ -319,3 +319,20 @@ const largestNumber = nums => {
     
     return nums.join('');
 };
+
+const removeElement = (nums, val) => {
+    let j = nums.length - 1;
+    while (nums[j] === val) {
+        nums.pop()
+        j--;
+    }
+    for (let i = 0; i < j; i++) {
+        if (nums[i] === val) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            while (nums[j] === val) {
+                nums.pop()
+                j--;
+            }
+        }
+    }
+};
