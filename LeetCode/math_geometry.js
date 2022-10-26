656781c05bd325c5f4443af2a0fd2d0dabdc7a31
Jonathan <jonathan.diaz714@gmail.com>
2022-10-26 15:15:05 -0400

    solved the isPalidnrome problem

--- LeetCode/math_geometry.js
+++ LeetCode/math_geometry.js
@@ -9,3 +9,20 @@ const isUgly = n => {
     
     return n === 1
 };
+
+const isPalindrome = x => {
+    if (x < 0) return false;
+    let i = 10 ** Math.floor(Math.log10(x));
+    let j = 1;
+    
+    while (i >= 10 ** j) {
+        let left = Math.floor(x / i) % 10;
+        i /= 10;
+        let right = Math.floor(x % (10 ** j) / (10 ** (j - 1)))
+        j++;
+        
+        if (left !== right) return false
+    }
+    
+    return true;
+};
