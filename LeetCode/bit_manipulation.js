9ab6cf3eb3fcb7b575e28b5826eb031a5e4004ea
Jonathan <jonathan.diaz714@gmail.com>
2022-10-27 02:01:44 -0400

    solved the add binary problem

--- LeetCode/bit_manipulation.js
+++ LeetCode/bit_manipulation.js
@@ -12,3 +12,39 @@ const reverse = x => {
         return 0
     }
 };
+
+const addBinary = (a, b) => {
+    let carry = 0;
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let res = '';
+    
+    while (i >= 0 || j >= 0 || carry) {
+        let d1 = a[i] ? parseInt(a[i]) : 0
+        let d2 = b[j] ? parseInt(b[j]) : 0
+        let sum = d1 + d2 + carry;
+        
+        switch (sum) {
+            case 3:
+                res = 1 + res;
+                carry = 1;
+                break;
+            case 2:
+                res = 0 + res;
+                carry = 1;
+                break;
+            case 1:
+                res = 1 + res;
+                carry = 0;
+                break
+            case 0:
+                res = 0 + res;
+                carry = 0;
+                break
+        }
+        i--;
+        j--;
+    }
+    
+    return res;
+};
