69e1c569014cfd480d08d53848cb65fc6475fbe0
Jonathan <jonathan.diaz714@gmail.com>
2022-10-27 17:02:09 -0400

    solved the integer to roman problem

--- LeetCode/math_geometry.js
+++ LeetCode/math_geometry.js
@@ -99,3 +99,34 @@ const myPow = (x, n) => {
     
     return n > 0 ? res : 1 / res
 };
+
+const intToRoman = num => {
+    let numerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ]
+    
+    let roman = '';
+    
+    numerals.forEach(n => {
+        let [letter, val] = n;
+        //I know that I need to concatinate the letter
+        while (num / val >= 1) {
+            roman += letter;
+            num -= val;
+        }
+    })
+    
+    return roman;
+};
