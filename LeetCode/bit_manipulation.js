a5fb8b99d6de4b6b36d5c6151d662a1d6ceb7f94
Jonathan <jonathan.diaz714@gmail.com>
2022-10-27 03:05:58 -0400

    solved the roman to integer problem

--- LeetCode/bit_manipulation.js
+++ LeetCode/bit_manipulation.js
@@ -75,9 +75,35 @@ const addBinary = (a, b) => {
     return res;
 };
 
+//solved with gauases formula
 const missingNumber = nums => {
     let sum = nums.reduce((acc, ele) => acc + ele, 0);
     let n = nums.length
     let expected = n * (n + 1) / 2;
     return expected - sum;
+};
+
+const romanToInt = s => {
+    let obj = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    let num = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]] < obj[s[i + 1]]) {
+            num += obj[s[i+ 1]] - obj[s[i]];
+            i++;
+        } else {
+            num += obj[s[i]];
+        }
+    }
+    
+    return num;
 };
