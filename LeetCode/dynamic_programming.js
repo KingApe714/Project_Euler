e341e26911ad119d673b20b5fbfed2177b2e44c0
Jonathan <jonathan.diaz714@gmail.com>
2022-10-28 23:35:34 -0400

    solved the coinChange problem

--- LeetCode/dynamic_programming.js
+++ LeetCode/dynamic_programming.js
@@ -7,3 +7,22 @@ const climbStairs = (n, memo={}) => {
     
     return memo[n];
 };
+
+const coinChange = (coins, amount, memo={}) => {
+    if (amount === 0) return 0;
+    if (amount < 0) return -1;
+    if (amount in memo) return memo[amount]
+    
+    
+    
+    let min = Infinity;
+    for (let coin of coins) {
+        let res = coinChange(coins, amount - coin, memo)
+        
+        if (res !== -1) min = Math.min(min, res);
+    }
+    
+    memo[amount] = min === Infinity ? -1 : 1 + min;
+    
+    return memo[amount];
+};
