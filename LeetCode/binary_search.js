67c8547d0d88059bbb432f1f7b47ef5faa1312df
Jonathan <jonathan.diaz714@gmail.com>
2022-10-29 02:54:47 -0400

    solved the valid perfect square problem

--- LeetCode/binary_search.js
+++ LeetCode/binary_search.js
@@ -60,3 +60,22 @@ const sortedSquares = nums => {
     
     return arr.reverse();
 };
+
+const isPerfectSquare = num => {
+    let l = 1;
+    let r = num;
+    
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+        
+        if (mid ** 2 > num) {
+            r = mid - 1;
+        } else if (mid ** 2 < num) {
+            l = mid + 1;
+        } else {
+            return true;
+        }
+    }
+    
+    return false;
+};
