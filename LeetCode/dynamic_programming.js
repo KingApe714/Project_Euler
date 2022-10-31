eb80c8fdfd33cb4bb77e6c976b4a084688fd8fb2
Jonathan <jonathan.diaz714@gmail.com>
2022-10-30 20:33:19 -0400

    solved the numSquares with the bottom up dp

--- LeetCode/dynamic_programming.js
+++ LeetCode/dynamic_programming.js
@@ -61,3 +61,16 @@ const stoneGame = piles => {
     
     return dp(0, 0, piles.length - 1)
 };
+
+const numSquares = n => {
+    const dp = [0];
+    
+    for (let i = 1; i <= n; i++) {
+        dp[i] = Number.MAX_VALUE;
+        for (let j = 1; j*j <= i; j++) {
+            dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+        }
+    }
+    
+    return dp[n]
+};
